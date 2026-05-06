2b8a6d044736e4d781e6069af59187dde4aa582a
0xCrypt0nic <n.greneche45@gmail.com>
2026-05-06 20:54:16 +0200

    refactor : suppression page paramètres et bouton settings, mode sombre forcé permanent

--- CerfApp/App.xaml.cs
+++ CerfApp/App.xaml.cs
@@ -7,8 +7,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-		var saved = Preferences.Get("app_theme", "dark");
-		UserAppTheme = saved == "light" ? AppTheme.Light : AppTheme.Dark;
+		UserAppTheme = AppTheme.Dark;
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
--- CerfApp/Controls/HeaderView.xaml.cs
+++ CerfApp/Controls/HeaderView.xaml.cs
@@ -9,9 +9,6 @@ public partial class HeaderView : ContentView
     public static readonly BindableProperty ShowBackProperty =
         BindableProperty.Create(nameof(ShowBack), typeof(bool), typeof(HeaderView), true);
 
-    public static readonly BindableProperty ShowSettingsProperty =
-        BindableProperty.Create(nameof(ShowSettings), typeof(bool), typeof(HeaderView), true);
-
     public string PageTitle
     {
         get => (string)GetValue(PageTitleProperty);
@@ -24,12 +21,6 @@ public partial class HeaderView : ContentView
         set => SetValue(ShowBackProperty, value);
     }
 
-    public bool ShowSettings
-    {
-        get => (bool)GetValue(ShowSettingsProperty);
-        set => SetValue(ShowSettingsProperty, value);
-    }
-
     public HeaderView()
     {
         InitializeComponent();
@@ -38,10 +29,5 @@ public partial class HeaderView : ContentView
     private async void OnBackTapped(object? sender, TappedEventArgs e)
     {
         await Navigation.PopAsync();
-    }
-
-    private async void OnSettingsClicked(object? sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new ParametresPage());
     }
 }
